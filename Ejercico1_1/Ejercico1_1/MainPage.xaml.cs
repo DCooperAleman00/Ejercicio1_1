d710663fa2ea813b6ef8ed143b6b6d25a6a90198
danny <danny@DESKTOP-PIUU10N>
2022-10-21 22:53:54 -0600

    Agregar archivos de proyecto.

---
+++ Ejercico1_1/Ejercico1_1/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercico1_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSumar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Double total = 0;
+                total = Double.Parse(txtNumero1.Text) + Double.Parse(txtNumero2.Text);
+                await Navigation.PushAsync(new resultado(total.ToString()));
+            }
+            catch
+            {
+                await DisplayAlert("Warning", "Ingrese los 2 numeros", "OK");
+            }
+        }
+
+        private async void btnRestar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Double total = 0;
+                total = Double.Parse(txtNumero1.Text) - Double.Parse(txtNumero2.Text);
+                await Navigation.PushAsync(new resultado(total.ToString()));
+            }
+
+
+            catch
+            {
+                await DisplayAlert("Warning", "Ingrese los 2 numeros", "OK");
+            }
+        }
+
+        private async void btnDividir_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Double total = 0;
+                total = Double.Parse(txtNumero1.Text) / Double.Parse(txtNumero2.Text);
+                await Navigation.PushAsync(new resultado(total.ToString()));
+            }
+            catch
+            {
+                await DisplayAlert("Warning", "Ingrese los 2 numeros", "OK");
+            }
+
+        }
+
+        private async void btnMultiplicar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Double total = 0;
+                total = Double.Parse(txtNumero1.Text) * Double.Parse(txtNumero2.Text);
+                await Navigation.PushAsync(new resultado(total.ToString()));
+            }
+            catch
+            {
+                await DisplayAlert("Warning", "Ingrese los 2 numeros", "OK");
+            }
+        }
+    }
+}
---
+++ Ejercico1_1/Ejercico1_1/resultado.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercico1_1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class resultado : ContentPage
+    {
+        public resultado()
+        {
+            InitializeComponent();
+        }
+        public resultado(String resultado)
+        {
+            InitializeComponent();
+            lblMostrarResultado.Text = "Resultado: " + resultado;
+
+        }
+
+    }
+}
